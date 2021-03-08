93f7b3d4cdaf93810aca4df605c8b5dfe81c3a90
DualLine-Kim <80057816+DualLine-Kim@users.noreply.github.com>
2021-03-08 21:32:23 +0900

    Create javabasic0308_1.java

---
+++ javabasic0308_1.java
@@ -0,0 +1,14 @@
+public class Main {
+
+public static void main(String[] args) {
+
+for (int m=2; m < 10; m++) {
+			System.out.println("--- " + m + " 단 ---");
+
+			for (int n=1; n < 10 ; n++) {
+				System.out.println(m + " x " + n + " = " + (m*n));
+
+			}
+		}
+	}
+}
